53207b011c9896c1e8547e9923db8889309bdd0b
Vasundara05 <113960614+Vasundara05@users.noreply.github.com>
2025-03-20 10:30:48 +0530

    Update hiding the folder.py

--- hiding the folder.py
+++ hiding the folder.py
@@ -5,4 +5,4 @@ secure_folder = r"C:\Users\derpt\OneDrive\SecureFolder"
 # Completely remove access for all users
 os.system(f'icacls "{secure_folder}" /deny Everyone:(OI)(CI)F')
 
-print("🔒 SecureFolder is now fully locked. Manual access is blocked.")
+print("SecureFolder is now fully locked. Manual access is blocked.")
